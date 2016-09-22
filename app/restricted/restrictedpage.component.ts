f20e53851e104c0207115bac36b490c24a1f0d1e
Leuschner <k021414@kl.kdo.int>
2016-09-22 15:46:00 +0200

    Route

--- app/restricted/restrictedpage.component.ts
+++ app/restricted/restrictedpage.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   template: `
     <h1>Restricted Page</h1>
+    <p>Nur sichtbar, wenn angemeldet...</p>
     `
 })
 export class RestrictedpageComponent { }
